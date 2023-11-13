fc39c216fc5c26b11b6e5fc0f3cba8b1972ec2ba
balazsova <139120071+balazsova@users.noreply.github.com>
2023-11-13 07:39:52 +0100

    Add files via upload

---
+++ docs/assets/cviceni7/cviceni07-files/cgs.m
@@ -0,0 +1,21 @@
+function [Q,R] = cgs(A)
+
+[m,n] = size(A);
+Q = zeros(m,min(m,n));
+R = zeros(min(m,n),n);
+
+for i = 1:n
+    z = A(:,i);
+    for j = 1:min(m,i-1)
+        R(j,i) = Q(:,j)'*z;
+    end
+    for j = 1:min(m,i-1)
+        z = z-Q(:,j)*R(j,i);
+    end
+    if i<=m
+        R(i,i) = norm(z);
+        Q(:,i) = z/R(i,i);
+    end
+end
+
+plotqr(A,Q,R,'cgs');
