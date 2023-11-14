c0d8f61daba408e912cfd777ac392a9148e826d8
Jan Papez <papez@math.cas.cz>
2023-11-14 13:16:28 +0100

    Add files via upload

--- docs/assets/cviceni07-files/Arnoldi_pro_vetsi_matici.m
+++ docs/assets/cviceni07-files/Arnoldi_pro_vetsi_matici.m
@@ -1,5 +1,6 @@
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% zvol vetsi ulohu
+% Vypocet baze Krylovova prostoru II - pro vetsi matici
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 n = 1000;
 
@@ -8,13 +9,11 @@ x = rand(n,1);
 
 [V,H] = Arnoldicgs(A,x,n); 
 
-% Ukol: vykresli ||I - V_k'*V_k|| pro k=1,2, ...,1000 a sleduj ztratu OG v ArnoldiCGS
+% Úkol: vykresli ||I - V_k'*V_k|| pro k=1,2, ...,1000 a sleduj ztratu OG v ArnoldiCGS
 
-% reseni:
-
-Acgs_ztrataog =      % inicializace
-for i = 1:n,
-    Acgs_ztrataog(i)  =   % dopln 
-end;
+Acgs_ztrataog = zeros(1,n);     % inicializace
+for i = 1:n
+    Acgs_ztrataog(i)  =   % doplò 
+end
 
 semilogy(Acgs_ztrataog,'*') 
--- docs/assets/cviceni07-files/baze_pro_symetrickou_matici.m
+++ docs/assets/cviceni07-files/baze_pro_symetrickou_matici.m
@@ -9,25 +9,9 @@ norm(A-A')                               % overeni symetrie
 
 x = rand(100,1);                         % startovni vektor
 
-% konstrukce baze K_10(A, x) Arnoldiho procesem
+% Konstrukce báze K_10(A, x) Arnoldiho procesem
 
 [V,H] = Arnoldimgs(A,x,10);              % OG dobre zachovana
 
-% jsou koeficienty v pravem hornim rohu H nulove?
-
-spy(H)                                 % nejsou
-pause;
-surf(H)                                % sleduj velikost prvku ... je mala
-pause;
-
-H1 = H(1:9,:);                           % zahozeni posledniho radku H
-norm(H1-H1')                             % odchylka H1 od symetrie ... je mala 
-pause;
-
-% konstrukce baze K_10(A, x) Lanc. tridiagonalizaci
-
-help lanc                                % porovnej kod s Arnoldim
-
-[V,T] = lanc(A,x,10);                    % ztrata OG v matici V   
-pause;
-[V,T] = lanc(A,x,100);                   % OG zcela ztracena  
+% Jsou koeficienty v pravém horním rohu H nulové?
+% Vytvoøte z H ètvercovou matici a ovìøte její symetrii pomocí normy
