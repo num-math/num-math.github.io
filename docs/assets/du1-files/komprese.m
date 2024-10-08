38aaa977fff8a883ab6ad7f711324155e4ec403f
Jan Papez <papez@math.cas.cz>
2024-10-08 14:52:58 +0200

    DU1 - doplneno "figure"

--- docs/assets/du1-files/komprese.m
+++ docs/assets/du1-files/komprese.m
@@ -22,6 +22,7 @@ for i=1:k
     X = X + U(:,i)*S(i,i)*V(:,i)';           % konstrukce dat
 end
 
+figure(1)
 imagesc(X)                  % vykresleni
 axis image
 colormap(gray)
@@ -31,5 +32,6 @@ n_doubles = (size(U,1)+size(V,1)+1)*k;       % vypocet velikosti komprimovaneho 
 n_kbytes = n_doubles*8/1024;
 fprintf('Pro ulozeni obrazku je pouzito %d [doubles] = %1.2f [kB]',n_doubles,n_kbytes)
 
+figure(2)
 semilogy(1:k,s(1:k),'.r',k+1:length(s),s(k+1:length(s)),'.k')
 title('singularni cisla')
