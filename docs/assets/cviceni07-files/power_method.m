4a77ab31df74675ba43145842644dc18e8574bbe
Michal Outrata <outrata@KNM06.MSEKCE>
2024-11-11 12:06:41 +0100

    upload files for cviko7

--- docs/assets/cviceni07-files/power_method.m
+++ docs/assets/cviceni07-files/power_method.m
@@ -1,19 +1,19 @@
 function [mu,v,res_2norm] = power_method(A,v0,niter)
 
-mu = zeros(niter+1,1);         % posloupnost approximaci nejvetsiho vl. cisla
-v = zeros(length(v0),niter+1); % posloupnost approximaci prislusneho vlastniho vektoru
-res_2norm = zeros(niter+1,1);  % normy residua || Av_k - mu_kv_k ||
+mu = nan(niter+1,1);         % posloupnost approximaci nejvetsiho vl. cisla
+v = nan(length(v0),niter+1); % posloupnost approximaci prislusneho vlastniho vektoru
+res_2norm = nan(niter+1,1);  % normy residua || Av_k - mu_kv_k ||
 
-v0 = v0/norm(v0);
-w = A*v0;
-v(:,1) = w/norm(w);
+%%% prvni ulozena data odpovidaji hodnotam pocatecniho vektoru
+v(:,1) = v0/norm(v0);
 mu(1) = v(:,1)'*A*v(:,1);
 res_2norm(1) = norm(A*v(:,1)-mu(1)*v(:,1));
 
+%%% nasledne postupujeme jako u Algoritmu 1 ze cviceni
 for k = 2:niter+1
-    w = 		% A*v_{k-1}
-    v(:,k) =	% normalizace vektoru w
-    mu(k) = 	% Rayleighuv podil 		
-    res_2norm(k) = norm(A*v(:,k)-mu(k)*v(:,k));
+    w =  		     % A*v_{k-1}
+    v(:,k) =	     % normalizace vektoru w
+    mu(k) =          % Rayleighuv podil 		
+    res_2norm(k) =    norm(A*v(:,k)-mu(k)*v(:,k)); % reziduum
 end
 end
--- docs/assets/cviceni07-files/power_method_test.m
+++ docs/assets/cviceni07-files/power_method_test.m
@@ -1,42 +1,36 @@
 close all;
- A = diag([4,3,2,1]);        
-% A = diag([4,1.2,1.1,1]);    
-% A = diag([4,3.9,2,1]);      
-% A = diag([4,-3.9,2,1]);     
-% A = diag([-4,3,2,1]);       
-% A = diag([4,4,2,1]);        
-% A = diag([4,-4,2,1]);       
-% A = rand(4);
+
+
+% matice
+A = diag([4,3,2,1]);
 
 % pocatecni vektor
- v0 = [1,1,1,1]';
-% v0 = [0.01,1,1,1]';       
-% v0 = rand(4,1);
+v0 = [1,1,1,1]';
+
 
 % pocet iteraci
-niter = 25;
+niter = 35;
 
-[mu,v,res_2norm] = power_method(A,v0,niter); 
+[mu,v,res_2norm] = power_method_reseni(A,v0,niter); 
 
-eig_dom = eigs(A,1);    % dominantni vlastni cislo spoctene Matlabem
-err = abs(mu-eig_dom);  % chyba approximace
-
+err_eigval = nan(1,size(v,2)); err_eigvec = nan(1,size(v,2)); % inicializace
+[eigvec_dom,eigval_dom] = eigs(A,1);    % dominantni vlastni cislo spoctene Matlabem
+for k = 1:size(v,2)
+    err_eigval(k) = abs(mu(k)-eigval_dom);  % chyba approximace vl. c.
+    err_eigvec(k) = norm(eigvec_dom-v(:,k));  % chyba approximace vl. v.
+end
 
 figure(1)
 subplot(221);
 plot(0:niter,mu,LineWidth=2);
-title('Aproximace vlastniho cisla','FontSize',14);
-xlabel('iterace');
+title('Aproximace $\mu_k$ dominantniho vlastniho cisla $\lambda_1$','FontSize',16, 'interpreter', 'latex');
+xlabel('iterace $k$','FontSize',14, 'interpreter', 'latex');  xlim([-0.5,niter+1]);
 
 subplot(223);
-semilogy(0:niter,err,LineWidth=2);
-title('Chyba aproximace vlastniho cisla ','FontSize',14);
-xlabel('iterace');
-
-subplot(224);
 semilogy(0:niter,res_2norm,LineWidth=2);
-title('Norma residua','FontSize',14);
-xlabel('iterace');
+title('Norma residua $\|Av_k - \mu_k v_k\|$','FontSize',16, 'interpreter', 'latex');
+xlabel('iterace $k$','FontSize',14, 'interpreter', 'latex');  xlim([-0.5,niter+1]);
+
 
 subplot(222);
 plot(0:niter,v(1,:),'.-',MarkerSize=20,LineWidth=2);
@@ -47,7 +41,12 @@ plot(0:niter,v(3,:),'.-',MarkerSize=20,LineWidth=2);
 hold on
 plot(0:niter,v(4,:),'.-',MarkerSize=20,LineWidth=2);
 hold on
-title('Aproximace vlastniho vektoru','FontSize',14);
-xlabel('iterace');
-ylabel('slozky vektoru','FontSize',14);
-legend('1','2','3','4');
+title('Aproximace $v_k$ vlastniho vektoru $s_1$','FontSize',16, 'interpreter', 'latex');
+xlabel('iterace $k$','FontSize',14, 'interpreter', 'latex'); xlim([-0.5,niter+1]);
+legend('$(v_k)_1$','$(v_k)_2$','$(v_k)_3$','$(v_k)_4$','FontSize',14, 'interpreter', 'latex');
+
+subplot(224);
+semilogy(0:niter,err_eigvec,LineWidth=2);
+title('Chyba aproximace $\|v_k - s_1\|$','FontSize',16, 'interpreter', 'latex');
+xlabel('iterace $k$','FontSize',14, 'interpreter', 'latex');  xlim([-0.5,niter+1]);
+
